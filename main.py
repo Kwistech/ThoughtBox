4a7d3c8dd41d61b1827961a1c4f5a22f3e63e1c2
Johnathon Kwisses <johnkwisses@gmail.com>
2016-12-09 21:51:52 -0700

    add doctrings and comments

--- main.py
+++ main.py
@@ -13,12 +13,15 @@ from random import choice
 
 
 class MyFloatLayout(FloatLayout):
+    """Class for FloatLayout - main layout that houses the app."""
+    # Class Variables - called as root.variable_name.
     text = StringProperty()
-    main_color = 0.3215, 0.5333, 0.8588, 1
+    main_color = 0.3215, 0.5333, 0.8588, 1  # Colors are RGBA.
     sub_color = 0.7725, 0.7725, 0.7725, 1
 
     def __init__(self):
-        # Class Variables
+        """Initiate Class variables and create popup display."""
+        # Class Variables (not accessible for thoughtbox.kv)
         super(FloatLayout, self).__init__()
         self.filename = "box.json"
         self.text = "ThoughtBox"
@@ -36,11 +39,21 @@ class MyFloatLayout(FloatLayout):
                            size_hint=(.4, .4))
 
     def get_json(self):
+        """Get data from JSON file (self.filename).
+
+        Returns:
+            dict: JSON data.
+        """
         with open(self.filename) as f:
             f = json.load(f)
         return f
 
     def set_json(self, text_input):
+        """Set text_input to JSON data and 'saves' it.
+
+        Args:
+            text_input (TextInput): User-entered text.
+        """
         json_data = self.get_json()
         if text_input.text:
             json_data["Thoughts"].append(text_input.text)
@@ -48,18 +61,23 @@ class MyFloatLayout(FloatLayout):
         with open(self.filename, "r+") as f:
             json.dump(json_data, f, indent=4)
 
+        # Closes Popup display
         self.popup.dismiss()
 
     def change_text(self):
+        """Change on-screen text to a random user-entered value."""
         json_data = self.get_json().values()
         self.text = choice(json_data[0])
 
     def add_thought(self):
+        """Open Popup for a user to enter a thought."""
         self.popup.open()
 
 
 class ThoughtBoxApp(App):
+    """Class for building app."""
     def build(self):
+        """Build MyFloatLayout()."""
         return MyFloatLayout()
 
 if __name__ == "__main__":
